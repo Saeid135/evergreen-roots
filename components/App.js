b94c19f21418532d2f07746051378c6aad081ab2
anasazeemi <anas.azeemi02@gmail.com>
2023-03-03 10:06:56 -0800

    Basic REACT Scaffolding

---
+++ components/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}

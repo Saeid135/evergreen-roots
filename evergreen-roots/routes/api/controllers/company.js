3715642af829dc705f3968b33ae04e799b653d04
Saeid Mohamed <saeid135@uw.edu>
2023-05-16 14:43:38 -0700

    add get call to company.js

--- evergreen-roots/routes/api/controllers/company.js
+++ evergreen-roots/routes/api/controllers/company.js
@@ -4,7 +4,7 @@ import sessions from 'express-session';
 var router = express.Router();
 
 router.post('/', async (req, res) => {
-    try{
+    try {
         const newCompany = new req.models.Company({
             type: req.body.type,
             name: req.body.name,
@@ -14,11 +14,48 @@ router.post('/', async (req, res) => {
             about: req.body.about
         })
         await newCompany.save();
-        res.send({"status":"success"})
+        res.send({ "status": "success" })
     }
-    catch(error){
+    catch (error) {
         console.log(error)
-        res.status(500).json({"status":"error", "error":error})
+        res.status(500).json({ "status": "error", "error": error })
+    }
+})
+
+router.get('/', async function (req, res) {
+    try {
+        let arrJson = []
+        let allCompanies = await req.models.Company.find()
+        let companyData = await Promise.all(
+            allCompanies.map(async company => {
+                try {
+                    let type = company.type
+                    let name = company.name
+                    let address = company.address
+                    let number = company.number
+                    let email = company.email
+                    let about = company.about
+                    let id = company._id
+                    let jsonElement = {
+                        "type": type,
+                        "name": "" + name,
+                        "address": "" + address,
+                        "number": number,
+                        "email": "" + email,
+                        "about": "" + about,
+                        "id": id
+                    }
+                    arrJson.push(jsonElement)
+                } catch (error) {
+                    console.log(error)
+                    res.status(500).json({ "status": "error", "error": error })
+                }
+            })
+        );
+        res.send(arrJson)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ "status": "error", "error": error })
     }
 })
 

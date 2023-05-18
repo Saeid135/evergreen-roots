ffc4a217b96636ecf95b76b5b1cff007bd7865ee
Saeid Mohamed <saeid135@uw.edu>
2023-05-17 19:07:43 -0700

    add searchTerm() function to register.js. Add more edits to index.html. Include additional images to img folder

--- evergreen-roots/public/javascripts/register.js
+++ evergreen-roots/public/javascripts/register.js
@@ -33,3 +33,46 @@ async function addCompany(){
 async function returnHome(){
     location.replace("./index.html")
 }
+
+async function searchTerm() {
+    document.getElementById("eliminate").style.display = "none";
+    let orgName = document.getElementById("search_term").value;
+    let companyJson = await fetchJSON(`api/company/search?name=${orgName}`)
+    let companyHtml = `
+            `
+    companyHtml += companyJson.map(companyInfo => {
+        let htmlCompanies = `
+        <div class="v3_107">
+            <div class="v3_108">
+                <span class="v3_109">${escapeHTML(companyInfo.name)}</span>
+            </div>
+            <div class="v3_110">
+                <a href="./index.html">
+                    <div class="v3_111"></div>
+                </a>
+                <span class="v3_112">Employees:</span>
+                <span class="v3_113">Select
+                    employees you want to message/send a request form </span>
+                <div class="v3_114">
+                    <div class="v3_115"></div>
+                    <span class="v3_116">Name: James Evans
+                        Job: Outreach Communications Lead
+                        Phone Number: 123-456-7890
+                        Email: james.evans@uw.edu</span>
+                </div>
+            </div>
+            <div class="v3_120"></div>
+            <div class="v3_121">
+                <p class="extra_detail"><b>Address:</b> ${escapeHTML(companyInfo.address)}</p>
+                <p class="extra_detail"><b>Email:</b> ${escapeHTML(companyInfo.email)}</p>
+                <p class="extra_detail"><b>Phone Number:</b> ${escapeHTML(companyInfo.number.toString())}</p>
+                <p class="extra_detail" style="line-height:2.5em;"><b>About:</b> ${escapeHTML(companyInfo.about)}
+                </p>
+            </div>
+        </div>
+        `
+        return htmlCompanies;
+    }).join("\n");
+    companyHtml += `</div>`
+    document.getElementById("show_orgs").innerHTML = companyHtml;
+}
